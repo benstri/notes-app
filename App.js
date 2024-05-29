45a45feca81ad117f08a0c9b6096bc7ac1fb6a95
Ben Strickland <bpjstrickland1@gmail.com>
2024-05-28 21:22:02 -0300

    attempt to add data storing

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-import { SafeAreaView, Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-native-reanimated'; 
-import { Button } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import MasonryList from "@react-native-seoul/masonry-list";
 import { TouchableOpacity } from 'react-native';
-import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation } from './db';
+import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation, useUpdateNoteMutation } from './db';
 
 function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
 
@@ -45,7 +44,7 @@ function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
       <Text style={tw`mb-4 text-xl text-white font-bold`}> 
         Inscript - Notes App
       </Text>
-      
+    
       {searchData ?
         <MasonryList
           style={tw`px-0.5 pt-4 pb-20`}
@@ -67,45 +66,41 @@ function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
   );
 }
 
-function NewNote({ route, navigation }) { // NOTE SCREEN PAGE
-
-  const [text, onChangeText, setText] = useState('New Note Title');
-  const [number, onChangeNumber] = useState('');
+function NewNote({ navigation, item }) { // NOTE SCREEN PAGE
+  const [updateNote] = useUpdateNoteMutation();
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   const inputRef = useRef(null);
-  const {note} = route.params;
 
   function focusInput() {
     inputRef.current.focus();
   }
 
-  /*
-  useLayoutEffect(() => {
-    navigation.setOptions({ title: route.params.data.title });
-  }, []); 
-  */
-
   // style later
 
   return (
-    <View style={tw`pt-5 pl-5 pr-5`}>
-      
+    <View style={tw`pt-5 pl-5 pr-5 bg-[#3F72AF] h-full`}>
       <TextInput
-        onChangeText={onChangeText}
-        value={text}
-        style={tw`text-2xl`}
+        onChangeText={(text) => setTitle(text)}
+        value={title}
+        style={tw`text-2xl text-white`}
         ref={inputRef}
+        placeholder='New Note Title'
       />
       <TextInput
-        onChangeText={onChangeNumber}
-        value={number}
+        onChangeText={(text) => setContent(text)}
+        value={content}
         placeholder='Add the content to your new note!'
-        style={tw`text-lg h-screen`}
-        keyboardType="numeric"
+        style={tw`text-lg h-4/5 text-white`}
         ref={inputRef}
         multiline={true}
-        
       />
-      
+      <TouchableOpacity
+        onPress={() => updateNote(item)}
+        style={tw`bg-[#46B5D1] absolute bottom-[5%] right-8 mx-auto items-center flex-1 justify-center rounded p-1`}
+      >
+        <Text style={tw`text-white text-center text-2xl mt--1`}>Save</Text>
+      </TouchableOpacity>
     </View>
   );
 }
