d1d108a951ec980ddcd7004da9515a8ed63fe802
Ben Strickland <bpjstrickland1@gmail.com>
2024-06-04 23:25:47 -0300

    finishing touches + added comments

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import { store } from './store';
 import 'react-native-reanimated'; 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { useState, useRef, useEffect, useLayoutEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import MasonryList from "@react-native-seoul/masonry-list";
 import { TouchableOpacity } from 'react-native';
 import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation, useUpdateNoteMutation } from './db';
+
+// above is all of the imports
 
 function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
   const [search, setSearch] = useState("");
@@ -18,24 +20,24 @@ function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
   const inputRef = useRef(null);
 
   
-  useEffect(() => {
+  useEffect(() => { // navigates to new note upon creating it
     if (addNoteData != undefined) {
       console.log(addNoteData.title);
       navigation.navigate("Note", {note: addNoteData});
     }
   }, [addNoteData]);
 
-  const renderItem = ({ item }) => (
+  const renderItem = ({ item }) => ( // note card inside the masonrylist
     <TouchableOpacity onPress={() => {navigation.navigate('Note', {note : item}); }} style={tw`w-[98%] mb-0.5 mx-auto bg-[#DBE2EF] p-1 rounded px-1`}> 
-      <Text style={tw`text-xl font-bold`}>
-        {item.title} 
+      <Text style={tw`text-xl font-bold`}> 
+        {item.title}
       </Text>
       <Text style={tw`text-lg`}>
-        {item.content}
+        {item.content} 
       </Text>
       <TouchableOpacity onPress={() => deleteNote(item)} style={tw`items-start`}> 
-        <Text style={tw`p-1 text-base bg-[#F67280] mb-1 rounded`}>
-          Delete
+        <Text style={tw`p-1 text-base bg-[#F67280] mb-1 rounded`}> 
+          Delete 
         </Text>
       </TouchableOpacity>
     </TouchableOpacity>
@@ -43,10 +45,10 @@ function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
     
   return (
     <View style={tw`flex-1 items-center pt-5 bg-[#3F72AF]`}> 
-      <Text style={tw`mb-4 text-xl text-white font-bold`}> 
-        Inscript - Notes App
-      </Text>
-      <TextInput
+      <Text style={tw`mb-4 text-xl text-white font-bold`}>
+        Inscript - Notes App 
+      </Text> 
+      <TextInput // search bar
         placeholder='Search'
         value={search}
         ref={inputRef}
@@ -55,7 +57,7 @@ function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
       />
     
       {searchData ?
-        <MasonryList
+        <MasonryList // columed notes
           style={tw`px-0.5 pt-4 pb-20`}
           data={searchData}
           numColumns={2}
@@ -65,7 +67,7 @@ function HomeScreen({ navigation, item }) { // HOME SCREEN PAGE
         />
         : <></>
       }
-      <TouchableOpacity
+      <TouchableOpacity // adds new note to the masonrylist
         onPress={() => { addNote({title: "", content: ""}); }}
         style={tw`bg-[#46B5D1] rounded-full absolute bottom-[5%] right-8 mx-auto items-center flex-1 justify-center w-12 h-12`}
       >
@@ -83,26 +85,24 @@ function NewNote({ route }) { // NOTE SCREEN PAGE
   const inputRef = useRef(null);
   
 
-  function focusInput() {
+  function focusInput() { // focuses on text input, makes it more user friendly
     inputRef.current.focus();
   }
 
-  saveNote = () => {
+  saveNote = () => { // save note function
     updateNote({id : note.id, content : content, title : title});
   }
 
-  // style later
-
   return (
     <View style={tw`pt-5 pl-5 pr-5 bg-[#3F72AF] h-full`}>
-      <TextInput
+      <TextInput // title of note
         onChangeText={(text) => setTitle(text)}
         value={title}
         style={tw`text-2xl text-white`}
         ref={inputRef}
         placeholder='New Note Title'
       />
-      <TextInput
+      <TextInput // content of note
         onChangeText={(text) => setContent(text)}
         value={content}
         placeholder='Add the content to your new note!'
@@ -110,7 +110,7 @@ function NewNote({ route }) { // NOTE SCREEN PAGE
         ref={inputRef}
         multiline={true}
       />
-      <TouchableOpacity
+      <TouchableOpacity // save note button
         onPress={saveNote}
         style={tw`bg-[#46B5D1] absolute bottom-[5%] right-8 mx-auto items-center flex-1 justify-center rounded p-1`}
         onSubmitEditing={() => {
